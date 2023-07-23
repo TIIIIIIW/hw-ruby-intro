7a83a0e3bc44921af0bd8606dca83b7e95a453ce
TIIIIIIW <109777077+TIIIIIIW@users.noreply.github.com>
2023-07-23 19:10:19 +0700

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -5,16 +5,27 @@
 def sum arr
   # YOUR CODE HERE
   sum = 0
-  arr.each {|x| sum+=x}
+  if arr.length != 0
+    arr.each {|x| sum+=x}
+  end
   sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  sum = 0
+  if arr.length > 2
+    arr.sort! {|x, y| y <=> x}
+    sum = arr[0]+arr[1]
+  else
+    sum = arr.sum
+  end
+  sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  
 end
 
 # Part 2
